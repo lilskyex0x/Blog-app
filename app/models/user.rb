5d5c0f967d1eb1455250f6c522ec04dc4b966575
unknown <lilskyex0x.apk@gmail.com>
2023-12-01 18:58:03 +0630

    Changes as reviewed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, foreign_key: 'author_id'
 
-  has_many :comment, foreign_key: 'author_id'
-  has_many :like, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
 
   def three_recent_posts
     Post.where(author: self).order(created_at: :desc).first(3)

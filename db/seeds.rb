bf05655f3b14cdff19d47dcbf3b975aebbdb4876
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-06 02:10:56 +0630

    Create data samples with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create some users
+5.times do
+    User.create(
+      name: Faker::Name.name,
+      photo: Faker::Internet.url,
+      bio: Faker::Lorem.paragraph
+    )
+  end
+  
+  # Create some posts for each user
+  User.all.each do |user|
+    3.times do
+      user.posts.create(
+        title: Faker::Lorem.sentence,
+        text: Faker::Lorem.paragraph,
+        comments_counter: 0,
+        likes_counter: 0
+      )
+    end
+  end
+  
+  # Create some comments and likes for each post
+  Post.all.each do |post|
+    5.times do
+      post.comments.create(
+        text: Faker::Lorem.sentence,
+        user_id: User.pluck(:id).sample
+      )
+      post.likes.create(
+        user_id: User.pluck(:id).sample
+      )
+    end
+    post.update(
+      comments_counter: post.comments.count,
+      likes_counter: post.likes.count
+    )
+  end
+  
+  # Update posts_counter for each user
+  User.all.each do |user|
+    user.update(posts_counter: user.posts.count)
+  end

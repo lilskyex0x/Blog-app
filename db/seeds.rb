7ed76124941e27a109403e85a2a2b41666f83704
unknown <lilskyex0x.apk@gmail.com>
2023-12-07 19:35:11 +0630

    Re-create database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,36 @@
-# Create some users
-5.times do
-    User.create(
-      name: Faker::Name.name,
-      photo: Faker::Internet.url,
-      bio: Faker::Lorem.paragraph
+# Delete all existing users
+User.delete_all
+
+# Create 5 users
+(1..5).each do |i|
+  User.create(
+    name: Faker::Name.name,
+    bio: Faker::Lorem.sentence,
+    photo: "user#{i}.jpg"
+  )
+end
+
+# Create some posts for each user
+User.all.each do |user|
+  rand(1..5).times do
+    user.posts.create(
+      title: Faker::Lorem.sentence(word_count: 3),
+      text: Faker::Lorem.paragraph,
+      comments_counter: 0,
+      likes_counter: 0
     )
   end
-  
-  # Create some posts for each user
-  User.all.each do |user|
-    3.times do
-      user.posts.create(
-        title: Faker::Lorem.sentence,
-        text: Faker::Lorem.paragraph,
-        comments_counter: 0,
-        likes_counter: 0
-      )
-    end
-  end
-  
-  # Create some comments and likes for each post
-  Post.all.each do |post|
-    5.times do
-      post.comments.create(
-        text: Faker::Lorem.sentence,
-        user_id: User.pluck(:id).sample
-      )
-      post.likes.create(
-        user_id: User.pluck(:id).sample
-      )
-    end
-    post.update(
-      comments_counter: post.comments.count,
-      likes_counter: post.likes.count
+end
+
+# Create some comments and likes for each post
+Post.all.each do |post|
+  rand(1..5).times do
+    post.comments.create(
+      text: Faker::Lorem.sentence,
+      user_id: User.pluck(:id).sample
+    )
+    post.likes.create(
+      user_id: User.pluck(:id).sample
     )
   end
-  
-  # Update posts_counter for each user
-  User.all.each do |user|
-    user.update(posts_counter: user.posts.count)
-  end
+end

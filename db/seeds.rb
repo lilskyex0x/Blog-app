a0d52cd18a8e60c7666b28b45d9432ff340b4247
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-08 15:25:03 +0630

    Update seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,35 +2,48 @@
 User.delete_all
 
 # Create 5 users
-(1..5).each do |i|
-  User.create(
-    name: Faker::Name.name,
-    bio: Faker::Lorem.sentence,
-    photo: "user#{i}.jpg"
-  )
+users = [
+  { name: "Eichi", bio: "Eichi is very popular among web developers. Her smile kills people", photo: "user1.jpg" },
+  { name: "Violux", bio: "She's a DJ who always make crowd go wild", photo: "user2.jpg" },
+  { name: "Wade", bio: "Wade is a professior at Cambridge university. His mathematics make people question about their lives", photo: "user3.jpg" },
+  { name: "Judie", bio: "She's a model from Vogue. A sexy one. People scream their heart out for her.", photo: "user4.jpg" },
+  { name: "Eddie", bio: "He's just a desperate kid but plot twist? He is spider-man", photo: "user5.jpg" },
+]
+
+# Create each user
+users.each do |user_data|
+  User.create(user_data)
 end
 
 # Create some posts for each user
 User.all.each do |user|
-  rand(1..5).times do
-    user.posts.create(
-      title: Faker::Lorem.sentence(word_count: 3),
-      text: Faker::Lorem.paragraph,
+  5.times do |i|
+    post = user.posts.create(
+      title: "Post #{i+1} by #{user.name}",
+      text: "This is post #{i+1} by #{user.name}",
       comments_counter: 0,
       likes_counter: 0
     )
-  end
-end
 
-# Create some comments and likes for each post
-Post.all.each do |post|
-  rand(1..5).times do
-    post.comments.create(
-      text: Faker::Lorem.sentence,
-      user_id: User.pluck(:id).sample
-    )
-    post.likes.create(
-      user_id: User.pluck(:id).sample
+    # Create some comments for each post
+    rand(1..5).times do |j|
+      post.comments.create(
+        text: "Comment #{j+1} on #{post.title}",
+        user_id: User.pluck(:id).sample
+      )
+    end
+
+    # Create some likes for each post
+    rand(1..5).times do
+      post.likes.create(
+        user_id: User.pluck(:id).sample
+      )
+    end
+
+    # Update comments_counter and likes_counter for the post
+    post.update(
+      comments_counter: post.comments.count,
+      likes_counter: post.likes.count
     )
   end
 end

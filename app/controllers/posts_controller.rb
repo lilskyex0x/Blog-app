fab20a38b08eb484846183dfb5da60a82997938f
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-08 18:43:15 +0630

    Create 2 new controller for comments and likes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,33 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.where.not(id: nil)
+    @new_post = @user.posts.build
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post was successfully created.'
+    else
+      flash[:alert] = @post.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end

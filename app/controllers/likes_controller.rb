7de40bafb8914b95ec401518477cbd85f665f593
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-14 16:14:35 +0630

    Modified controller actions

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,6 +3,6 @@ class LikesController < ApplicationController
     @post = Post.includes(:likes).find(params[:post_id])
     @like = @post.likes.create(user_id: current_user.id)
 
-    redirect_to @post
+    redirect_to user_post_path(@post.author, @post)
   end
 end

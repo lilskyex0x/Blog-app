7edcf45ce13401b9198305bb3045831ab9566e3a
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-12-14 19:54:40 +0630

    Make changes as review from partner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,27 +7,27 @@ class UsersController < ApplicationController
     @user = User.includes(:posts).find(params[:id])
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to @user
+  #   else
+  #     render 'new'
+  #   end
+  # end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update(user_params)
+  #     redirect_to @user
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
-  private
+  # private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :image)
-  end
+  # def user_params
+  #   params.require(:user).permit(:name, :email, :password, :image)
+  # end
 end
